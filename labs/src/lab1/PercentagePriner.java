74cc64a6d9e68d0d39be6538ccbc5cfe0fb66a31
= <=>
2014-10-10 16:13:04 +0100

    task 8 do not know ....

---
+++ labs/src/lab1/PercentagePriner.java
@@ -0,0 +1,15 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class PercentagePriner {
+    public static void main(String[] args) {
+        double probability = 8.70;
+        int percentage = (int) (100 * probability);
+        System.out.println(percentage);
+    }
+}
