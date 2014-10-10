01f79cb12f1bebf103f70a49650338deec7f5e28
= <=>
2014-10-10 18:44:10 +0100

    task 16, done!

--- labs/src/lab1/Task16.java
+++ labs/src/lab1/Task16.java
@@ -13,23 +13,27 @@ public class Task16 {
     public static void main(String[] arg){
         Scanner sc = new Scanner(System.in);
         System.out.println("type value for string s1");
-        //String s1 = sc.next();\
-        String s1 = "abcd";
+        String s1 = sc.next();
+        //String s1 = "abcd";
         System.out.println("type value for string s2");
-        //String s2 = sc.next();
-        String s2 = "wxyz";
+        String s2 = sc.next();
+        //String s2 = "wxyz";
         System.out.println("type value for string s3");
-        //String s3 = sc.next();
-        String s3 = "pqrs";
+        String s3 = sc.next();
+        //String s3 = "pqrs";
         if (s1.compareTo(s2) <= 0){
             if(s2.compareTo(s3) <=0){
                 System.out.println("s2 is the middlest  " + s2);
-            }else System.out.println("s3 is the middlest  " + s3);
+            }else if (s1.compareTo(s3) <=0){
+                System.out.println("s3 is the middlest  " + s3);
+            }else System.out.println("s1 is the middlest  " + s1);
+                    
         }else {
             if(s1.compareTo(s3) <= 0){
                 System.out.println("s1 is the middlest  " + s1);
-            }else System.out.println("s3 is the smolest  " + s3);
+            }else if (s2.compareTo(s3) <= 0){
+                System.out.println("s3 is the middlest  " + s3);
+            }else System.out.println("s2 is the middlest  " + s2);
         }
     }
-        
-}
+}   
