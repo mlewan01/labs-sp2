bc709416fbe843da274eca3dfe2843adf5aeb049
= <=>
2014-10-10 21:37:37 +0100

    task 30 !

---
+++ labs/src/lab1/Task30.java
@@ -0,0 +1,20 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class Task30 {
+    public static void main(String[] arg){
+        
+        //for (int i = 1; i <= 10; i++) System.out.println(i + " squared equals " + i * i);
+        
+        int i = 1;
+        while(i<=10){
+            System.out.println(i + " squared equals " + i * i);
+            i++;
+        }
+    }
+}
