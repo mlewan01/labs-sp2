75341ba2a368b7bd734e68cfafa75c06b302cf05
= <=>
2014-10-10 20:22:10 +0100

    task 20 -22

---
+++ labs/src/lab1/SimpleLoop.java
@@ -0,0 +1,19 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class SimpleLoop {
+    public static void main(String[] args) {
+        int i = 6;
+        int limit = 100;
+        while (i < limit) {
+            System.out.println("i = " + i);
+            i++;
+            i++;
+        }
+    }
+}
