f07770466a2a1f4572a0099aaebda374a30b683f
= <=>
2014-10-10 15:59:02 +0100

    task 6, good question! whay is printing only 36.0....

---
+++ labs/src/lab1/AverageCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class AverageCalculator {
+    public static void main(String[] arg){
+        int age1 = 18;
+        int age2 = 35;
+        int age3 = 50;
+        int age4 = 44;
+        double averageAge = (age1 + age2 + age3 + age4)/4;
+        System.out.println(age1 + age2 + age3 + age4+ " / 4 = "+averageAge);
+        long d = 3/4;
+        System.out.println(d);
+    }
+}
