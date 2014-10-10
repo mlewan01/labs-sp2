4e29c060b679e33cc8b858ea9bf09963cefee63b
= <=>
2014-10-10 08:24:37 +0100

    test 11.10, my be this time (not!)

---
+++ labs/src/lab1/HelloPrinter.java
@@ -0,0 +1,21 @@
+package lab1;
+
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+public class HelloPrinter {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
+/*
+* files contained in the directory after compiling the HelloPrinter.java file are:
+* - HelloPrinter.java
+* - HelloPrinter.class
+* output of the program is "Hello, World"
+* file .class contains java byte code
+*/
