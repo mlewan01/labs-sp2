68b8ed0d04cec3c3799169bc6f0de3c91cf68b6f
= <=>
2014-10-10 16:03:13 +0100

    task 6, good question! whay is printing only 36.0.... aaaaaaaaa....!

---
+++ labs/src/lab1/Task7.java
@@ -0,0 +1,18 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class Task7 {
+    public static void main(String[] arg){
+        int age1 = 18;
+        int age2 = 35;
+        int age3 = 50;
+        int age4 = 44;
+        double averageAge = (age1 + age2 + age3 + age4+0.0)/4;
+        System.out.println(age1 + age2 + age3 + age4+ " / 4 = "+averageAge);
+    }
+}
