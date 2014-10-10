9467e2d5972d7d811aa5fd1ac0d724777d33d06d
= <=>
2014-10-10 21:13:11 +0100

    task 26

---
+++ labs/src/lab1/Task26.java
@@ -0,0 +1,18 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class Task26 {
+    public static void main(String[] arg){
+        for(int i=1; i<6; i++){
+            for(int j=1; j<i+1; j++){
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+}
