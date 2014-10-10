c619cd7d871febb66bb9a423f44079b557db9a7b
= <=>
2014-10-10 19:41:47 +0100

    task 17. system overheating!

---
+++ labs/src/lab1/Task17.java
@@ -0,0 +1,26 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+
+import java.util.Scanner;
+
+public class Task17 {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type value for string s1");
+        String s1 = sc.next();
+        //String s1 = "abcd";
+        System.out.println("type value for string s2");
+        String s2 = sc.next();
+        //String s2 = "wxyz";
+        System.out.println("type value for string s3");
+        String s3 = sc.next();
+        //String s3 = "pqrs";
+        System.out.println("error, error. \n system overhiting");
+    }
+}
