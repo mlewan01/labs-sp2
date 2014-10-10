4a2f8430cab80bb372611ae783d5013fff74ce23
= <=>
2014-10-10 21:20:03 +0100

    task27

---
+++ labs/src/lab1/PrintSum.java
@@ -0,0 +1,26 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+
+import java.util.Scanner;
+
+public class PrintSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        int n = 1;
+        do {
+            System.out.print("Please enter a number, 0 to quit: ");
+            n = in.nextInt();
+            if (n != 0) {
+                sum = sum + n;
+                System.out.println("Sum = " + sum);
+            }
+        }while (n != 0);
+    }
+}
