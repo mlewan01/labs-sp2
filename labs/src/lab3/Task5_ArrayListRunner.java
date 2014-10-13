6cda858810e7adc47736be87c7d2a917c9aaa535
= <=>
2014-10-13 08:04:41 +0100

    Task5, lab3,Array List, started

---
+++ labs/src/lab3/Task5_ArrayListRunner.java
@@ -0,0 +1,17 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab3;
+
+import java.util.ArrayList;
+
+public class Task5_ArrayListRunner {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        System.out.println(names);
+    }
+}
