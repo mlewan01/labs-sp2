5be3b417ed58fdd6e86ddc696fbfc51d98394266
= <=>
2014-10-13 01:08:10 +0100

    increment demo, done

---
+++ labs/src/lab1/IncrementDemo.java
@@ -0,0 +1,18 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab1;
+public class IncrementDemo {
+    public static void main(String[] arg){
+        int x = 10;
+        x = x +1;
+        System.out.println(x);
+        int y = -3;
+        y++;
+        System.out.println(y);
+    }
+}
