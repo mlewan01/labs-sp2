af99cd8995f8011e02dac16b39ca80458cad9c6c
= <=>
2014-10-13 07:36:51 +0100

    Task3, Lab3, CustomerLister, complited !

---
+++ labs/src/lab3/CustomerLister.java
@@ -0,0 +1,16 @@
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+package lab3;
+public class CustomerLister {
+    public static void main(String[] args){
+        String[] s = {"Cathy", "Ben", "Jorge", "Wanda", "Freddie"};
+        for(String is: s){
+            System.out.println(is);
+        }
+    }
+}
