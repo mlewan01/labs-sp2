28efe5ecda8a3f1bd901defb3f7bde4a0ac06051
= <=>
2014-10-13 08:22:27 +0100

    Task5, lab3,Array List, done !

--- labs/src/lab3/Task5_ArrayListRunner.java
+++ labs/src/lab3/Task5_ArrayListRunner.java
@@ -13,5 +13,29 @@ public class Task5_ArrayListRunner {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
         System.out.println(names);
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Connie");
+        names.add("David");
+        names.add("Edward");
+        names.add("Fran");
+        names.add("Gomez");
+        names.add("Harry");
+        System.out.println("after adding names: " + names);
+        System.out.println("first name with get: "+names.get(0));
+        System.out.println("last name with the get: "+names.get(names.size()-1));
+        names.set(0,"Alice B Toklas");
+        System.out.println("list after change to first element: "+names);
+        names.add(4, "Doug");
+        System.out.println("printing the list with the enchanced for loop !!!:");
+        for(String s: names){
+            System.out.println(s);
+        }
+        
+        ArrayList<String> names2 = new ArrayList<String>(names);
+        System.out.println("printing array 2: "+names2);
+        names.remove(0);
+        System.out.println("printing array 1: "+names);
+        System.out.println("printing array 2: "+names2);
     }
 }
