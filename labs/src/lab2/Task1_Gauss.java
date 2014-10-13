600694572ceea389bb139a4d7019c305ed0146ac
= <=>
2014-10-13 02:33:38 +0100

    task 1, done!

---
+++ labs/src/lab2/Task1_Gauss.java
@@ -0,0 +1,33 @@
+package lab2;
+
+
+import java.util.Scanner;
+
+/*
+ * Copy rights Mariusz Lewandowski
+ * http://www.artemlux.com
+ * @author: mlwan01 <Mariusz Lewandowski, Student ref: 12906023>
+ * class: sp2-2014
+ */
+
+public class Task1_Gauss {
+    public static void main(String[] arg){
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("please enter amount of loops");
+        int j = s.nextInt();
+        int sum = 0;
+        for(int i=1;i<=j;i++){
+            sum+=i;
+            //System.out.println(i);
+        }
+        int ii = 1;
+        int sum2 = 0;
+        while(ii<=j){
+            sum2+=ii;
+            ii++;
+        }
+        System.out.println("loop \"for\" sum   : " + sum);
+        System.out.println("loop \"while\" sum   : " + sum2);
+    }
+}
